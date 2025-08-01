7628da01dac27c09cbd49242696d459ac4655769
Volodymyr Dedenko <dedenkof@ukr.net>
2025-08-01 20:44:30 +0300

    2sec44-46less Outputting User-specific Task, Task Data in the Task Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,11 @@ import { HeaderComponent } from './header/header.component';
 import { UserComponent } from "./user/user.component";
 import { TasksComponent } from "./tasks/tasks.component";
 import { DUMMY_USERS } from "./dummy-users";
-import { NgFor } from '@angular/common';
-import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserComponent, TasksComponent, NgFor, NgIf],
+  imports: [HeaderComponent, UserComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
